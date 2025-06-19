ffcad567337096597f4995cc7c7bb44d5a28ccc5
Pritam Das <pritam.aber@gmail.com>
2025-06-20 00:49:03 +0530

    final fix

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD4WTNqrKo8BHKOb7R4M8KM777sERoBC-E",
-  authDomain: "tagfuse-8a198.firebaseapp.com",
-  projectId: "tagfuse-8a198",
-  storageBucket: "tagfuse-8a198.firebasestorage.app",
-  messagingSenderId: "491993381991",
-  appId: "1:491993381991:web:e2eb944629ec3b6c81e8c2",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
